15d368faffc544ba3fd34e7a6b32de97455e6302
Doc <doc@actten.com>
2021-04-08 19:25:38 -0700

    define algo in js file for main gameplay loop

---
+++ scripts/scripts.js
@@ -0,0 +1,31 @@
+console.log("this is my test")
+
+// computerPlay
+//     pick randomly from rock paper scissor
+//     return that result
+
+// computerSelection
+//     runs computerPlay, not sure why TOP seperates this?
+//     return answer
+
+// playerSelection
+//     offer dialog asking for rock paper scissor
+//         be agnostic to case for spelling
+//         return error for mispell or abberant choice
+//     store answer
+//     return answer
+
+// playRound
+//     runs computerSelection
+//     runs playerSelection
+//     compares answers
+//         paper beats rock
+//         rock beats scissors
+//         scissors beats paper
+//     declares winner
+
+// playLoop
+//     hold playerWins
+//     hold computerWins
+//     loop through playRound for five wins
+//     declare champion
